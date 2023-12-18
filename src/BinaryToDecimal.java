1cdc84e3b4fa12a10eaf47ad15d67b7ebff54984
kartsevnik <kartsevnik@gmail.com>
2023-12-18 11:13:00 +0100

    add new task and solution of method BinaryToDecimal

---
+++ src/BinaryToDecimal.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class BinaryToDecimal {
+    public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Inserisci una stringa binaria:");
+        String binaryString = scanner.nextLine();
+
+        int decimalValue = BinaryToDecimal(binaryString);
+        System.out.println("Valore decimale: " + decimalValue);
+        scanner.close();
+    }
+
+    public static int BinaryToDecimal(String binaryString) {
+        int decimalValue = 0;
+
+        try {
+            decimalValue = Integer.parseInt(binaryString, 2);
+        } catch (NumberFormatException e) {
+            System.out.println("La stringa inserita non è binaria.");
+        }
+
+        return decimalValue;
+    }
+}
